2a4f43cfff5253108ccdecc5657fd5c859e632db
David Anthoff <anthoff@berkeley.edu>
2014-05-30 14:01:35 -0700

    Progress

--- src/perftest.jl
+++ src/perftest.jl
@@ -3,11 +3,11 @@ include("sneasy.jl")
 
 function doruns(model)
     for i=1:30000
-        run(566,model)
+        run(model)
     end
 end
 
 m = getsneasy()
-run(566,m)
+run(m)
 
 @time doruns(m)
