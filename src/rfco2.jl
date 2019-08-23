7d698017f2497e87cee5f86d35e9d9bef0b22f6e
David Anthoff <anthoff@berkeley.edu>
2019-08-23 15:32:29 -0700

    Update CO2 radiative forcing

--- src/rfco2.jl
+++ src/rfco2.jl
@@ -1,9 +1,15 @@
-module rfco2component
 using Mimi
 
 @defcomp rfco2 begin
-    deltat = Parameter()
-    atmco2 = Parameter(index=[time])
+ 	a₁      = Parameter()             # CO₂ forcing constant.
+    b₁      = Parameter()             # CO₂ forcing constant.
+    c₁      = Parameter()             # CO₂ forcing constant.
+    CO₂_0   = Parameter()             # Initial (pre-industrial) CO₂ concentration (ppm).
+	N₂O_0   = Parameter()             # Initial (pre-industrial) N₂O concentration (ppb).
+    N₂O     = Parameter(index=[time]) # N₂O concentration (ppb).
+
+    scale_CO₂ = Parameter() #Scaling factor to ensure "Tune the coefficient of CO2 forcing to acheive desired F2x, using # pre-industrial CO2 and N2O FOLLOWING FAIR APPROACH
+    CO₂ = Parameter(index=[time])
     rf_co2 = Variable(index=[time])
 end
 
@@ -11,7 +17,11 @@ function run_timestep(s::rfco2, t::Int)
     v = s.Variables
     p = s.Parameters
 
-    v.rf_co2[t] = 3.7 * log(p.atmco2[t]/p.atmco2[1])/log(2.0)
+ #CO₂ Radiative Forcing
+    CO₂_diff = p.CO₂[t]-p.CO₂_0
+    N_hat = 0.5 * (p.N₂O[t] + p.N₂O_0)
+
+    v.rf_co2[t] = ((p.a₁*CO₂_diff^2 + p.b₁*abs(CO₂_diff) + p.c₁*N_hat + 5.36) * log(p.CO₂[t] / p.CO₂_0)) * p.scale_CO₂
+    
 end
 
-end
